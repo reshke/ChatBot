0ab263fed7b2d84a738068cbb70b3a81bc3f21c1
ChizhovAlexey <43272109+ChizhovAlexey@users.noreply.github.com>
2018-10-09 08:26:05 +0500

    Tests for exception added.
    Added few tests to check, that program calls exception in incorrect situation.

--- src/Test/Test/StringGameTestsException.java
+++ src/Test/Test/StringGameTestsException.java
@@ -2,13 +2,87 @@ package Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.Rule;
 import org.junit.jupiter.api.Test;
+import org.junit.rules.ExpectedException;
 
-class StringGameTestsException {
+import main.Games.StringGuessGame;
 
+
+public class StringGameTestsException {
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void testZeroLengthLineException() {
+		try {
+			StringGuessGame game = new StringGuessGame(0);
+		}
+		catch(IllegalArgumentException e) {
+			return;
+		}
+		fail("Exception did not happened");
 	}
 
+	@Test
+	public void testTooBigLineException() {
+		try {
+			StringGuessGame game = new StringGuessGame(100000000);
+		}
+		catch(IllegalArgumentException e) {
+			return;
+		}
+		fail("Exception did not happened");
+	}
+	
+	@Test
+	public void testAskingLeftIsZeroException() {
+		StringGuessGame game = new StringGuessGame(10);
+		game.startGame();
+		try {
+			game.postQuery(0, 5);
+		}
+		catch(IllegalArgumentException e) {
+			return;
+		}
+		fail("Exception did not happened");
+	}
+	
+	@Test
+	public void testAnotherOrderArgumentsException() {
+		StringGuessGame game = new StringGuessGame(10);
+		game.startGame();
+		try {
+			game.postQuery(2, 1);
+		}
+		catch(IllegalArgumentException e) {
+			return;
+		}
+		fail("Exception did not happened");
+	}
+	
+	@Test
+	public void testRightPositionBeyondBoundException() {
+		StringGuessGame game = new StringGuessGame(10);
+		game.startGame();
+		try {
+			game.postQuery(4, 11);
+		}
+		catch(IllegalArgumentException e) {
+			return;
+		}
+		fail("Exception did not happened");
+	}
+	
+	@Test
+	public void testStoppingStoppedGameException() {
+		StringGuessGame game = new StringGuessGame(10);
+		game.startGame();
+		game.endGame();
+		try {
+			game.endGame();
+		}
+		catch(UnsupportedOperationException e) {
+			return;
+		}
+		fail("Exception did not happened");
+	}
 }
